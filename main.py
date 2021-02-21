5bbb81c54e5532a8a8c78263e147d95ae47800c3
d.kisel <d.kisel@mcart.ru>
2021-02-21 20:19:38 +0300

    Init

---
+++ main.py
@@ -0,0 +1,111 @@
+from fastapi import FastAPI, HTTPException
+import pendulum
+
+app = FastAPI()
+
+
+def city_dict_serializer(city_params: list) -> dict:
+    city_dict = {'geonameid': int(city_params[0]),
+                 'name': city_params[1],
+                 'asciiname': city_params[2],
+                 'alternatenames': city_params[3],
+                 'latitude': float(city_params[4]),
+                 'longtitude': float(city_params[5]),
+                 'feature_class': city_params[6],
+                 'feature_code': city_params[7],
+                 'county_code': city_params[8],
+                 'cc2': city_params[9],
+                 'admin1_code': city_params[10],
+                 'admin2_code': city_params[11],
+                 'admin3_code': city_params[12],
+                 'admin4_code': city_params[13],
+                 'population': int(city_params[14]),
+                 'elevation': city_params[15],
+                 'dem': city_params[16],
+                 'timezone': city_params[17],
+                 'modification_date': city_params[18][:-1]}
+    return city_dict
+
+
+with open('RU.txt', 'r', encoding='utf-8') as file:
+    cities_list = []
+    for line in file:
+        serialized_city = city_dict_serializer(line.split('\t'))
+        cities_list.append(serialized_city)
+
+
+def find_city_by_id(geoname_id: int):
+    first = 0
+    last = len(cities_list)
+
+    while first <= last:
+        mid = (first + last) // 2
+
+        if geoname_id < cities_list[mid].get('geonameid'):
+            last = mid - 1
+        elif geoname_id > cities_list[mid].get('geonameid'):
+            first = mid + 1
+        else:
+            city_dict = cities_list[mid]
+            return city_dict
+
+
+def find_city_by_name(city_name: str):
+    cities_by_name = []
+
+    for city in cities_list:
+        alter_names = city.get('alternatenames').split(',')
+        if city_name in alter_names:
+            cities_by_name.append(city)
+
+    cities_by_name.sort(key=lambda c: c.get('population'))
+    if not cities_by_name:
+        return None
+    result_city = cities_by_name[-1]
+    return result_city
+
+
+@app.get("/city_info")
+async def city_info(geoname_id: int):
+    city_dict = find_city_by_id(geoname_id)
+    return city_dict
+
+
+@app.get("/cities_page")
+async def cities_page(page: int, page_size: int):
+    page_from = (page - 1) * page_size
+    page_to = page * page_size
+
+    cities_page_list = cities_list[page_from:page_to]
+    return cities_page_list
+
+
+@app.get("/compare_cities")
+async def compare_cities(city_1: str, city_2: str):
+    city_1 = find_city_by_name(city_1)
+    city_2 = find_city_by_name(city_2)
+    if not city_1 or not city_2:
+        raise HTTPException(
+            status_code=404,
+            detail="City not found"
+        )
+
+    result = {
+        'city_1': city_1,
+        'city_2': city_2
+    }
+
+    if city_1.get('latitude') > city_2.get('latitude'):
+        result.update({'northern': city_1.get('name')})
+    else:
+        result.update({'northern': city_2.get('name')})
+
+    if city_1.get('timezone') == city_2.get('timezone'):
+        result.update({'same_timezone': True, 'time_difference': 0})
+    else:
+        city_1_time = pendulum.now(city_1.get('timezone'))
+        city_2_time = city_1_time.in_timezone(city_2.get('timezone'))
+        difference = abs(city_1_time.hour - city_2_time.hour)
+        result.update({'same_timezone': False, 'time_difference': difference})
+
+    return result
